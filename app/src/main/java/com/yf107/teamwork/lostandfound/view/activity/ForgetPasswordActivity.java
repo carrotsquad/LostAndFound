02c24ac020a454bbce4ffa574e3a757b03f6b998
Thread10969 <695488704@qq.com>
2020-04-14 09:01:12 +0800

    commit

--- app/src/main/java/com/yf107/teamwork/lostandfound/view/activity/ForgetPasswordActivity.java
+++ app/src/main/java/com/yf107/teamwork/lostandfound/view/activity/ForgetPasswordActivity.java
@@ -129,7 +129,7 @@ public class ForgetPasswordActivity extends AppCompatActivity implements IForget
             @NonNull
             @Override
             protected char[] getAcceptedChars() {
-                char[] c = {'@','a', 'b', 'c', 'd', 'e', 'f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0'};
+                char[] c = {'.','@','a', 'b', 'c', 'd', 'e', 'f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0'};
                 return c;
             }
 
